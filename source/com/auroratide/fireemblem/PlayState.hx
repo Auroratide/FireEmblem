69252350fda411ba0d1d119dcaa6bcd8a7e5c0b5
auroratide <tf.auroratide@gmail.com>
2016-09-04 23:34:15 -0500

    [Setup] Initialized project

---
+++ source/com/auroratide/fireemblem/PlayState.hx
@@ -0,0 +1,27 @@
+package com.auroratide.fireemblem;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState {
+
+/*  Constructor
+ *  =========================================================================*/
+    public function new() {
+        super();
+    }
+
+/*  Flixel API
+ *  =========================================================================*/
+    override public function create():Void {
+        super.create();
+    }
+
+    override public function update(elapsed:Float):Void {
+        super.update(elapsed);
+    }
+
+    override public function destroy():Void {
+        super.destroy();
+    }
+
+}
