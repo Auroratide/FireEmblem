3d904185846289bfceef6d3daf4cb3e603ab775e
auroratide <tf.auroratide@gmail.com>
2016-09-05 12:30:58 -0500

    [S8] Refactored MapCursor slightly

--- source/com/auroratide/fireemblem/map/MapCursor.hx
+++ source/com/auroratide/fireemblem/map/MapCursor.hx
@@ -10,12 +10,6 @@ class MapCursor extends FlxSprite {
     public function new(map:FeMap) {
         super();
         this.map = map;
-
-        offset = FlxPoint.get(
-            (Constants.CURSOR_PIXEL_WIDTH - Constants.TILE_PIXEL_WIDTH) / 2,
-            (Constants.CURSOR_PIXEL_HEIGHT - Constants.TILE_PIXEL_HEIGHT) / 2
-        );
-
         initGraphic();
     }
 
@@ -26,6 +20,11 @@ class MapCursor extends FlxSprite {
 /*  Private Methods
  *  =========================================================================*/
     private function initGraphic():Void {
+        offset = FlxPoint.get(
+            (Constants.CURSOR_PIXEL_WIDTH - Constants.TILE_PIXEL_WIDTH) / 2,
+            (Constants.CURSOR_PIXEL_HEIGHT - Constants.TILE_PIXEL_HEIGHT) / 2
+        );
+
         loadGraphic(Paths.CURSOR, true, Constants.CURSOR_PIXEL_WIDTH, Constants.CURSOR_PIXEL_HEIGHT);
         animation.add("idle", [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2]);
         animation.play("idle");
