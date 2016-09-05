507474bfc9fa243ee182105f0cb0e72ab61df684
auroratide <tf.auroratide@gmail.com>
2016-09-05 08:22:49 -0500

    [S1] The user can view a map generated from files.  S1 is submitted for QA.

--- source/com/auroratide/fireemblem/Meta.hx
+++ source/com/auroratide/fireemblem/Meta.hx
@@ -2,7 +2,7 @@ package com.auroratide.fireemblem;
 
 class Meta {
 
-    public static inline var VERSION = "0.0.1";
+    public static inline var VERSION = "0.0.1:160904";
     public static inline var AUTHORS = "Timothy Foster and Kevin Sharp";
 
 }
