bfe4ab8e6da8df57be68ce73a7fc633d9ce99dc0
auroratide <tf.auroratide@gmail.com>
2016-09-08 11:41:46 -0500

    [S8] Holding the directional keys allows the cursor to continuously move

--- source/com/auroratide/fireemblem/control/MapCursorControl.hx
+++ source/com/auroratide/fireemblem/control/MapCursorControl.hx
@@ -6,10 +6,14 @@ import com.auroratide.fireemblem.map.MapCursor;
 
 class MapCursorControl extends FlxBasic {
 
+    private static inline var DELAY_HOLD = 18;
+    private static inline var DELAY_REPEAT = 3;
+
     public function new(input:GameInput, cursor:MapCursor) {
         super();
         this.input = input;
         this.cursor = cursor;
+        this.holdCounter = 0;
     }
 
 /*  Flixel API
@@ -17,18 +21,39 @@ class MapCursorControl extends FlxBasic {
     override public function update(elapsed:Float):Void {
         super.update(elapsed);
 
-        if(input.up.justPressed)
-            cursor.up();
-        if(input.right.justPressed)
-            cursor.right();
-        if(input.down.justPressed)
-            cursor.down();
-        if(input.left.justPressed)
-            cursor.left();
+        if(canMove())
+            move();
+
+        updateHoldCounter();
     }
 
 /*  Private Members
  *  =========================================================================*/
     private var input:GameInput;
     private var cursor:MapCursor;
+    private var holdCounter:Int;
+
+/*  Private Methods
+ *  =========================================================================*/
+    private inline function move():Void {
+        if(input.up.pressed)
+            cursor.up();
+        if(input.right.pressed)
+            cursor.right();
+        if(input.down.pressed)
+            cursor.down();
+        if(input.left.pressed)
+            cursor.left();
+    }
+
+    private inline function canMove():Bool {
+        return holdCounter == 0 || (holdCounter > DELAY_HOLD && holdCounter % DELAY_REPEAT == 0);
+    }
+
+    private inline function updateHoldCounter():Void {
+        if(input.directions.pressed)
+            ++holdCounter;
+        if(input.directions.released)
+            holdCounter = 0;
+    }
 }
