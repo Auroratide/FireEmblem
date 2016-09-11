e3274731015a964bc13d36e2cf7099ed6777d17a
auroratide <tf.auroratide@gmail.com>
2016-09-11 07:08:02 -0500

    [Meta] updated version

--- source/com/auroratide/fireemblem/Meta.hx
+++ source/com/auroratide/fireemblem/Meta.hx
@@ -2,7 +2,7 @@ package com.auroratide.fireemblem;
 
 class Meta {
 
-    public static inline var VERSION = "0.0.3:160910";
+    public static inline var VERSION = "0.0.5:160911";
     public static inline var AUTHORS = "Timothy Foster and Kevin Sharp";
 
 }
